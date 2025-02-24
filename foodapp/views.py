073f2a88826872ca50193a7fb1aaf737d9fdce95
arya-2407 <aryapofficial@gmail.com>
2025-02-24 11:35:33 -0800

    playing around with templates

--- foodapp/views.py
+++ foodapp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
 def index(req):
     return HttpResponse("Hello World")
 
 def item(req):
-    return HttpResponse("Hello Item view")
+    item_list = Item.objects.all()
+    return render(req,'foodapp/index.html',{'items' : item_list})
+
+
+def detail(req,item_id):
+    item = Item.objects.get(pk=item_id)
+    return render(req,'foodapp/detail.html',{'item' : item})
