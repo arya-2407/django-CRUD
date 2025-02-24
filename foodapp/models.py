260e68151913e039e67efd525d0ff2222967795e
arya-2407 <aryapofficial@gmail.com>
2025-02-23 17:00:52 -0800

    creating models

--- foodapp/models.py
+++ foodapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_desc = models.CharField(max_length=100,blank=True)
+    item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
+    
--- foodapp/urls.py
+++ foodapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('item/',views.item,name='index'),
+    path('item/',views.item,name='item'),
 
 ]
