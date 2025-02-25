1f23335945654f40ef444980696acaeedfe34ff7
arya-2407 <aryapofficial@gmail.com>
2025-02-25 15:01:10 -0800

    sytling message

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 def register(req):
-    form = UserCreationForm()
+    if req.method == 'POST':
+        form = UserCreationForm(req.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(req,f'Welcome {username}, your account is created')
+            return redirect('foodapp:item')
+    else:
+        form = UserCreationForm()
     return render(req,'users/register.html',{'form':form})
