fa09eeda41c3d933f6e3ce37454d6d4644623265
arya-2407 <aryapofficial@gmail.com>
2025-02-25 16:55:32 -0800

    added login logout functionality

--- users/views.py
+++ users/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth import logout
 # Create your views here.
 
 def register(req):
     if req.method == 'POST':
-        form = UserCreationForm(req.POST)
+        form = RegisterForm(req.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(req,f'Welcome {username}, your account is created')
-            return redirect('foodapp:item')
+            return redirect('login')
     else:
-        form = UserCreationForm()
+        form = RegisterForm ()
     return render(req,'users/register.html',{'form':form})
+
+def custom_logout(request):
+    logout(request)
+    return redirect('foodapp:item')  # Show a logout confirmation page
