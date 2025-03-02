9aae793ff1c7921f1fccc2a86d7c16c3280e5ad3
arya-2407 <aryapofficial@gmail.com>
2025-03-01 16:36:32 -0800

    added Profile model to diplay profiles

--- foodproj/urls.py
+++ foodproj/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as authentication_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include('foodapp.urls')),
@@ -27,4 +30,4 @@ urlpatterns = [
     path('login/',authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/',user_views.custom_logout,name='logout'),
     path('profile/',user_views.profilepage,name='profile'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
